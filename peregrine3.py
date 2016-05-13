a8c9b39639691b5f220b041c4118e96bb9d57b8c
David <Atreusion@users.noreply.github.com>
2016-05-12 22:35:44 -0400

    Getting the basics smooshed out

--- peregrine3.py
+++ peregrine3.py
@@ -1,34 +1,71 @@
+# 19:37:58 Thursday, May 12, 2016
 import pydle
 
-class MyClient(pydle.Client):
-    """ This is a simple bot that will greet people as they join the channel. """
-    
-    def on_raw(self, message):
-        print(message)
+server_data = {
+'irc.chatspike.net' : {
+    'port' : 6697,
+    'nickname' : 'Peregrine3',
+    'channels' : ['#peregrine3'],
+    'server_password' : None,
+    'ns_password' : None,
+    'tls' : True
+    },
+'irc.freenode.net' : {
+    'port' : 6697,
+    'nickname' : 'Peregrine3',
+    'channels' : ['#peregrine3'],
+    'server_password' : None,
+    'ns_password' : None,
+    'tls' : True
+    }
+#'irc.twitch.tv' : {
+#    'port' : 6667,
+#    'nickname' : 'AtryBot',
+#    'channels' : ['#atreus11'],
+#    'password' : None
+#    }
+}
 
+class MyClient(pydle.Client):
     def on_connect(self):
         super().on_connect()
         # Can't greet many people without joining a channel.
-        self.join('#peregrine')
+        for channel in server_data[self.connection.hostname]['channels']:
+            self.join(channel)
 
     def on_join(self, channel, user):
         super().on_join(channel, user)
-        self.message(channel, 'Hey there, {user}!', user=user)
-    
+        msg = 'Hey there, %s!' % user
+        self.message(channel, msg)
+
     @pydle.coroutine
-    def on_message(self, target, source, message):
-        super().on_message(target, source, message)
-        if message == "!disconnect":
-            self.disconnect()
-        
+    def on_message(self, channel, user, message):
+        super().on_message(channel, user, message)
+        low_message = message.lower()
+        if low_message.startswith('!test'):
+            self.message(channel, "%s, this is a test in %s on %s." % (user, channel, self.connection.hostname))
+        if low_message == "!die":
+            print("IT WORKS I GUESS")
+            for client in pool:
+                self.disconnect(client)
+            exit()
+    def on_raw(self, message):
+        super().on_raw(message)
+        print(message)
 
 # Setup pool and connect clients.
 pool = pydle.ClientPool()
-servers = ['irc.chatspike.net',]
 
-for server in servers:
-    client = MyClient('Peregrine3', realname = "testbot")
-    pool.connect(client, server, 6697, tls=True)
+for server in server_data:
+    client = MyClient(server_data[server]['nickname'])
+    pool.connect(client, server, tls=server_data[server]['tls'], password=server_data[server]['server_password'])
 
 # Handle all clients in the pool at once.
-pool.handle_forever()
+try:
+    pool.handle_forever()
+except SystemExit:
+    pass
+except:
+    import traceback
+    print(traceback.format_exc())
+    input()
